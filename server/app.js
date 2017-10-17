dc37b73d1e54cdd33e16d6ea95c08beb6dc2361b
deepak-datwani <dep.light@gmail.com>
2017-10-17 08:53:39 +0530

    changes of ignoring build and remove extra comment marks

--- server/app.js
+++ server/app.js
@@ -6,16 +6,16 @@ const path = require('path');
 const app = express();
 
 // Setup logger
-//
+
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 //Always return the main index.html, so react-router render the route in theclint
-//
-app.get('*', (req, res)=>{
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 module.exports = app;
